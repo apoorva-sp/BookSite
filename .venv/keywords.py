e5d6df79ee7d0ef90b967038d01feefcae98b6c8
Aditya-Natarajan <aditya.natarajan1@gmail.com>
2024-10-13 23:30:45 +0530

    keywords file changed

--- .venv/keywords.py
+++ .venv/keywords.py
@@ -1,6 +1,6 @@
 class keywords:
-    username = "root"
-    password = "@D1tyanr"
+    username = "YOUR USERNAME"
+    password = "YOUR PASSWORD"
     database = "BookSite"
     host = "127.0.0.1"
     port =  3306
