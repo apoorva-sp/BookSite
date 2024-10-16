9041f8ff24247daf69ff8d950673cce0c8264da0
apoorva-sp <apoorvasp2003@gmail.com>
2024-10-16 16:08:45 +0530

    table errors fixed

--- src/Database/keywords.py
+++ src/Database/keywords.py
@@ -1,6 +1,6 @@
 class keywords:
-    username = "your name"
-    password = "Your password"
+    username = "root"
+    password = "apoorvasp2003@"
     database = "Booksite"
     host = "127.0.0.1"
     port =  3306
