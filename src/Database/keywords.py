9b345d582828d0648fc4d7e816110ab3f6146b6f
apoorva-sp <apoorvasp2003@gmail.com>
2024-10-16 16:54:31 +0530

    keywords updated

--- src/Database/keywords.py
+++ src/Database/keywords.py
@@ -1,6 +1,6 @@
 class keywords:
-    username = "root"
-    password = "apoorvasp2003@"
+    username = "username"
+    password = "your password"
     database = "Booksite"
     host = "127.0.0.1"
-    port =  3306
+    port =3306
