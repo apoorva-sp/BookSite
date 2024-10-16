328f95d93e3dc84d4e9f9ffc7e1e1a91d73b35af
Aditya-Natarajan <aditya.natarajan1@gmail.com>
2024-10-16 15:24:05 +0530

    Inital commit

--- src/Database/keywords.py
+++ src/Database/keywords.py
@@ -1,6 +1,6 @@
 class keywords:
-    username = "your name"
-    password = "Your password"
+    username = "root"
+    password = "@D1tyanr"
     database = "Booksite"
     host = "127.0.0.1"
-    port =  3306
+    port = 3306
