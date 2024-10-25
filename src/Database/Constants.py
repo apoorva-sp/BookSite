0df2a1f41eb7b5ce255a181385a8700069f80ae0
Aditya-Natarajan <aditya.natarajan1@gmail.com>
2024-10-25 22:15:25 +0530

    constants and dbconfig updated MemberDB completed

--- src/Database/Constants.py
+++ src/Database/Constants.py
@@ -1,4 +1,19 @@
 
 class Constants:
-    stausId1 = 1
-    statusMessage1 = "Insert Error"
+    status_id51 = 51
+    status_message51 = "Insert Error"
+
+    status_id52 = 52
+    status_message52 = "Preference update Error"
+
+    status_id53 = 53
+    status_message53 = "Name update Error"
+
+    status_id54 = 54
+    status_message54 = "Password update Error"
+
+    status_id55 = 55
+    status_message55 = "Address update Error"
+
+    status_id56 = 56
+    status_message56 = "Number update Error"
--- src/Database/MembersDB.py
+++ src/Database/MembersDB.py
@@ -4,18 +4,19 @@ from src.Beans.Member import Member
 from src.Beans.Status import Status
 from src.Database.Constants import Constants
 
+
 class MemberDB:
 
     def __init__(self, con: mysql.connector.connect()):
         self.con = con
         self.status = Status()
 
-    def insertMember(self, m: Member) -> Status:
+    def insert_member(self, m: Member) -> Status:
         try:
             cursor = self.con.cursor()
-            sql = ("INSERT INTO MEMBERS (phone,fullname,password,addressLineOne,addressLineTwo,city," +
+            sql = ("INSERT INTO MEMBERS (phone,fullname,password,address_line_one,address_line_two,city," +
                    "state,pincode,preferenceOne,preferenceTwo,preferenceThree)" +
-                   "VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)")
+                   "VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);")
             password = md5(m.password.encode())
             values = (
                 m.phone, m.name, password.hexdigest(), m.addressLine1, m.addressLine2, m.city, m.state,
@@ -23,13 +24,77 @@ class MemberDB:
             cursor.execute(sql, values)
 
         except Exception as e:
-            self.status = Status(Constants.statusId1, Constants.statusMessage1)
+            self.status = Status(Constants.status_id51, Constants.status_message51)
             print(e)
         return self.status
 
-    def updateName(self,PhoneNumber:str , newName :str):
+    def update_preference(self, phone_number: str, preference_one: str, preference_two: str, preference_three: str):
         try:
             cursor = self.con.cursor()
+            sql = ("UPDATE MEMBERS SET preferenceOne = (%s),preferenceTwo = (%s),preferenceThree = (%s)" +
+                   " WHERE phone = (%s);")
+            values = (preference_one, preference_two, preference_three, phone_number)
+            cursor.execute(sql, values)
+        except Exception as e:
+            self.status = Status(Constants.status_id52, Constants.status_message52)
+            print(e)
+        return self.status
 
+    def update_name(self, phone_number: str, new_name: str):
+        try:
+            cursor = self.con.cursor()
+            sql = "UPDATE MEMBERS SET fullname = (%s) WHERE phone = (%s);"
+            values = (new_name, phone_number)
+            cursor.execute(sql, values)
+        except Exception as e:
+            self.status = Status(Constants.status_id53, Constants.status_message53)
+            print(e)
+        return self.status
+
+    def update_password(self, phone_number: str, new_password: str):
+        try:
+            cursor = self.con.cursor()
+            sql = "UPDATE MEMBERS SET password = (%s) WHERE phone = (%s);"
+            password = md5(new_password.encode())
+            values = (password.hexdigest(), phone_number)
+            cursor.execute(sql, values)
+        except Exception as e:
+            self.status = Status(Constants.status_id54, Constants.status_message54)
+            print(e)
+        return self.status
+
+    def update_address(self, phone_number: str, address_line_one: str, address_line_two: str, city: str, state: str,
+                       pincode: str):
+        try:
+            cursor = self.con.cursor()
+            sql = ("UPDATE MEMBERS SET address_line_one = (%s), address_line_two = (%s), city = (%s)" +
+                   "state = (%s), pincode = (%s) WHERE phone = (%s);")
+            values = (address_line_one, address_line_two, city, state, pincode, phone_number)
+            cursor.execute(sql, values)
+        except Exception as e:
+            self.status = Status(Constants.status_id55, Constants.status_message55)
+            print(e)
+        return self.status
+
+    def select_all_members(self):
+        cursor = self.con.cursor()
+        sql = "SELECT * FROM MEMBERS;"
+        return cursor.execute(sql)
+
+    def delete_member(self, phone: str):
+        cursor = self.con.cursor()
+        sql = "DELETE FROM MEMBER WHERE phone = (%s);"
+        value = (phone,)
+        cursor.execute(sql, value)
+        return self.status
+
+    def update_number(self, old_number: str, new_number: str):
+        try:
+            cursor = self.con.cursor()
+            sql = "UPDATE MEMBERS SET phone = (%s) where phone = (%s);"
+            cursor.execute(sql, (new_number, old_number))
         except Exception as e:
             print(e)
+            self.status = Status(Constants.status_id56, Constants.status_message56)
+            print(e)
+        return self.status
