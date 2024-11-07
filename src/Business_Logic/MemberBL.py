fc6a2b88170b69d76df2f5977964fa77732380ad
Aditya-Natarajan <aditya.natarajan1@gmail.com>
2024-11-07 21:39:54 +0530

    minor correction of return statements in memberbl.py

--- src/Business_Logic/MemberBL.py
+++ src/Business_Logic/MemberBL.py
@@ -15,10 +15,8 @@ class MemberBL:
     def add_member(self, m1: Member) -> Status:
         if len(m1.phone) != 10:
             self.status = Status(Constants.status_id57, Constants.status_message57)
-            return self.status
         elif m1.preference1 == "":
             self.status = Status(Constants.status_id58, Constants.status_message58)
-            return self.status
         else:
             self.status = self.mdb.insert_member(m1)
 
@@ -26,15 +24,14 @@ class MemberBL:
             self.db.commit()
         else:
             self.db.rollback()
+
         return self.status
 
     def update_preference(self, phone: str, preference1: str, preference2: str, preference3: str) -> Status:
         if preference1 == "":
             self.status = Status(Constants.status_id58, Constants.status_message58)
-            return self.status
         elif preference2 == preference1 or preference1 == preference3 or preference2 == preference3:
             self.status = Status(Constants.status_id59, Constants.status_message59)
-            return self.status
         else:
             self.status = self.mdb.update_preference(phone, preference1, preference2, preference3)
 
@@ -42,13 +39,13 @@ class MemberBL:
             self.db.commit()
         else:
             self.db.rollback()
+
         return self.status
 
     def update_address(self, phone_number: str, address_line_one: str, address_line_two: str, city: str, state: str,
                        pincode: str) -> Status:
         if address_line_one == "" or state == "" or city == "" or pincode == "":
             self.status = Status(Constants.status_id60, Constants.status_message60)
-            return self.status
         else:
             self.status = self.mdb.update_address(phone_number, address_line_one, address_line_two, city, state,
                                                   pincode)
@@ -57,15 +54,14 @@ class MemberBL:
             self.db.commit()
         else:
             self.db.rollback()
+
         return self.status
 
     def update_password(self, new_password: str, old_password: str, phone: str) -> Status:
         if old_password == new_password:
             self.status = Status(Constants.status_id61, Constants.status_message61)
-            return self.status
         elif new_password == "":
             self.status = Status(Constants.status_id62, Constants.status_message62)
-            return self.status
         else:
             self.status = self.mdb.update_password(phone, new_password)
 
@@ -73,15 +69,14 @@ class MemberBL:
             self.db.commit()
         else:
             self.db.rollback()
+
         return self.status
 
     def update_number(self, new_phone: str, old_phone: str) -> Status:
         if old_phone == new_phone:
             self.status = Status(Constants.status_id63, Constants.status_message63)
-            return self.status
         elif len(new_phone) != 10:
             self.status = Status(Constants.status_id64, Constants.status_message64)
-            return self.status
         else:
             self.status = self.mdb.update_number(old_phone, new_phone)
 
@@ -89,12 +84,12 @@ class MemberBL:
             self.db.commit()
         else:
             self.db.rollback()
+
         return self.status
 
     def update_name(self, phone: str, new_name: str) -> Status:
         if new_name == "":
             self.status = Status(Constants.status_id65, Constants.status_message65)
-            return self.status
         else:
             self.status = self.mdb.update_name(phone, new_name)
 
@@ -102,4 +97,5 @@ class MemberBL:
             self.db.commit()
         else:
             self.db.rollback()
+
         return self.status
