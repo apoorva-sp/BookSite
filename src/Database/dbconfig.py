bbaee8e9569c684d53b2244976b92492de54e660
Aditya-Natarajan <aditya.natarajan1@gmail.com>
2024-12-16 22:09:39 +0530

    fixed the table issues

--- src/Database/dbconfig.py
+++ src/Database/dbconfig.py
@@ -19,27 +19,27 @@ class dbConfig:
             sql.append(""" 
                         CREATE TABLE IF NOT EXISTS members(
                             mID INT PRIMARY KEY AUTO_INCREMENT,
-                            phone VARCHAR(20) UNIQUE NOT NULL,
-                            fullname VARCHAR(20) NOT NULL,
+                            phone CHAR(10) UNIQUE NOT NULL,
+                            fullname VARCHAR(64) NOT NULL,
                             passkey VARCHAR(32) NOT NULL,
-                            addressLineOne VARCHAR(30) NOT NULL,
-                            addressLineTwo VARCHAR(30),
-                            city VARCHAR(30) NOT NULL,
-                            state VARCHAR(30) NOT NULL,
-                            pincode VARCHAR(10) NOT NULL,
-                            preferenceOne VARCHAR(20) NOT NULL,
-                            preferenceTwo VARCHAR(20) NOT NULL,
-                            preferenceThree VARCHAR(20) NOT NULL
+                            addressLineOne VARCHAR(128) NOT NULL,
+                            addressLineTwo VARCHAR(128),
+                            city VARCHAR(64) NOT NULL,
+                            state VARCHAR(64) NOT NULL,
+                            pincode VARCHAR(16) NOT NULL,
+                            preferenceOne VARCHAR(32) NOT NULL,
+                            preferenceTwo VARCHAR(32) NOT NULL,
+                            preferenceThree VARCHAR(32) NOT NULL
                         );
                         """)
             sql.append(""" 
                         CREATE TABLE IF NOT EXISTS books(
                             bID INT AUTO_INCREMENT PRIMARY KEY,
-                            title VARCHAR(50) NOT NULL,
-                            author VARCHAR(50) NOT NULL,
-                            description VARCHAR(100) NOT NULL,
+                            title VARCHAR(128) NOT NULL,
+                            author VARCHAR(128) NOT NULL,
+                            description VARCHAR(256) NOT NULL,
                             price INT NOT NULL,
-                            image VARCHAR(100) NOT NULL,
+                            image VARCHAR(256) NOT NULL,
                             seller_id INT NOT NULL,
                             FOREIGN KEY (seller_id) REFERENCES members(mID) 
                                 ON DELETE CASCADE 
@@ -80,7 +80,7 @@ class dbConfig:
             sql.append(""" 
                         CREATE TABLE IF NOT EXISTS category (
                             cID INT AUTO_INCREMENT PRIMARY KEY,
-                            category VARCHAR(50) NOT NULL
+                            category VARCHAR(64) NOT NULL
                         );
                         """)
             sql.append(""" 
@@ -96,7 +96,10 @@ class dbConfig:
                                 ON UPDATE CASCADE
                         );
                         """)
-
+            sql.append("""
+                        INSERT INTO CATEGORY(CATEGORY) VALUES('fantasy'),('mystery'),
+                        ('fiction'),('non-fiction'),('science'),('biography');
+                        """)
             for statement in sql:
                 cur.execute(statement)
             con.commit()
